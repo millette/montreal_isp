49d5f008f8454bd41f23a6075ac1fedf6d0a07d5
Robin Millette <robin@millette.info>
2016-10-04 17:22:51 -0400

    More functions.

--- utils/cli.js
+++ utils/cli.js
@@ -5,7 +5,20 @@ const utils = require('./')
 const data = require('../data/all_plans.json')
 
 const a = utils.toObjects(data)
+/*
 const b = utils.toArrays(a)
-
+const c = utils.sortObjects(a, 'up_speed')
+const d = utils.unlimitedObjects(a)
+const e = utils.byIspObjects(a, 'vif')
+const f = utils.types.map((t) => utils.byTypeObjects(a, t))
 console.log('a:', a)
 console.log('b:', b)
+console.log('c:', c)
+console.log('d:', d)
+console.log('f:', f)
+const g = utils.IspObjects(a)
+console.log('g:', g)
+*/
+
+const h = utils.byTypeObjects(a)
+console.log('hh:', h)
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
 // npm
+const flatten = require('lodash.flatten')
+const differenceBy = require('lodash.differenceby')
 const pickBy = require('lodash.pickby')
+const sortBy = require('lodash.sortby')
+const uniq = require('lodash.uniq')
 
 const makePlan = (isp, name, down_speed, up_speed, limit, price, link) => { return { isp, name, down_speed, up_speed, limit, price, link } } // eslint-disable-line camelcase
 const picker = (p) => pickBy(p, (v) => v)
@@ -10,6 +14,14 @@ const makePlanImp = (p) => makePlan.apply(null, p)
 const makePlanArray = (obj) => [obj.isp, obj.name, obj.down_speed, obj.up_speed, obj.limit, obj.price, obj.link].map(nuller)
 
 module.exports = {
+  types: ['cable', 'dsl', 'fttn', 'fibre', 'commuté'].sort(),
   toObjects: (d) => d.map(makePlanImp).map(picker),
-  toArrays: (objs) => objs.map(makePlanArray)
+  toArrays: (objs) => objs.map(makePlanArray),
+  sortObjects: (objs, p) => sortBy(objs, p).reverse(),
+  unlimitedObjects: (objs) => objs.filter((z) => !z.limit),
+  byIspObjects: (objs, isp) => objs.filter((z) => z.isp.toLowerCase() === isp.toLowerCase()),
+  IspObjects: (objs) => uniq(objs.map((z) => z.isp.toLowerCase())).sort(),
+  byTypeObjects: (objs, type) => type
+    ? objs.filter((z) => z.name.toLowerCase().replace(/â/g, 'a').replace(/fibe/, 'fibre').indexOf(type.toLowerCase()) !== -1)
+    : differenceBy(objs, flatten(module.exports.types.map((t) => module.exports.byTypeObjects(objs, t))), 'name')
 }
