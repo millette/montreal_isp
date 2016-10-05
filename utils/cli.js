40ff7afe5250679a373a385cde391f8964eabc5f
Robin Millette <robin@millette.info>
2016-10-05 11:16:00 -0400

    Touchups.

--- utils/cli.js
+++ utils/cli.js
@@ -11,14 +11,18 @@ const c = utils.sortObjects(a, 'up_speed')
 const d = utils.unlimitedObjects(a)
 const e = utils.byIspObjects(a, 'vif')
 const f = utils.types.map((t) => utils.byTypeObjects(a, t))
-console.log('a:', a)
 console.log('b:', b)
 console.log('c:', c)
 console.log('d:', d)
 console.log('f:', f)
 const g = utils.IspObjects(a)
 console.log('g:', g)
-*/
 
 const h = utils.byTypeObjects(a)
 console.log('hh:', h)
+
+console.log('a:', a)
+*/
+
+const i = utils.upRatioObjects(a)
+console.log('i:', i)
--- utils/index.js
+++ utils/index.js
@@ -12,6 +12,7 @@ const picker = (p) => pickBy(p, (v) => v)
 const nuller = (v) => v === undefined ? null : v
 const makePlanImp = (p) => makePlan.apply(null, p)
 const makePlanArray = (obj) => [obj.isp, obj.name, obj.down_speed, obj.up_speed, obj.limit, obj.price, obj.link].map(nuller)
+const byRatio = (field, objs) => sortBy(objs, (p) => (p[field] || 1e6) / p.price).reverse()
 
 module.exports = {
   types: ['cable', 'dsl', 'fttn', 'fibre', 'commuté'].sort(),
@@ -21,6 +22,9 @@ module.exports = {
   unlimitedObjects: (objs) => objs.filter((z) => !z.limit),
   byIspObjects: (objs, isp) => objs.filter((z) => z.isp.toLowerCase() === isp.toLowerCase()),
   IspObjects: (objs) => uniq(objs.map((z) => z.isp.toLowerCase())).sort(),
+  upRatioObjects: byRatio.bind(null, 'up_speed'),
+  downRatioObjects: byRatio.bind(null, 'down_speed'),
+  limitRatioObjects: byRatio.bind(null, 'limit'),
   byTypeObjects: (objs, type) => type
     ? objs.filter((z) => z.name.toLowerCase().replace(/â/g, 'a').replace(/fibe/, 'fibre').indexOf(type.toLowerCase()) !== -1)
     : differenceBy(objs, flatten(module.exports.types.map((t) => module.exports.byTypeObjects(objs, t))), 'name')
